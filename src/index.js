e1cec66416a05bffaae2b3162a28c4832744f035
josimarminetefera <josimaminete@gmail.com>
2020-12-14 19:43:08 -0300

    Adicionado teste de rota.

--- src/index.js
+++ src/index.js
@@ -11,5 +11,10 @@ app.use(body_parser.json());
 //QUANDO ENVIAR UMA REQUIZIÇÃO COM URL PARA PEGAR OS PAREMTROS
 app.use(body_parser.urlencoded({ extended: false }));
 
+//TESTAR SE O LINK ESTÁ FUNCIONANDO
+app.get("/", (req, res) => {
+    res.send("Oi");
+});
+
 //QUAL PORTA O APLICATIVO VAI ESCUTAR 
 app.listen(3000);
