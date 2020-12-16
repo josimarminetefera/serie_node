e72dae3b874c316850e7e3c8eb8bf74bc7921cb3
josimarminetefera <josimaminete@gmail.com>
2020-12-16 19:54:47 -0300

    Adicionado token nos envios de registrar e autenticar.

--- src/controllers/AutenticacaoController.js
+++ src/controllers/AutenticacaoController.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
+
+//IMPORTAR JSON DE CONFIGURACAO
+const autenticacao_configuracao = require("../config/autenticacao.json");
 
 //BUSCAR A CLASS USUARIO
 const Usuario = require("../models/Usuario");
 
 //ESTA ROTA AQUI É SÓ PARA USUARIO SÓ USADO AQUI DENTRO
 const rota = express.Router();
+
+function gerarToken(params = {}) {
+    //sign(valor sempre fixo id, hash unico no app, {quanto tempo ele expira})
+    //INFORMAÇÃO QUE VAI SER UNICA PARA O USUARIO ID EU USO PARA O TOKEN
+    return jsonwebtoken.sign(params, autenticacao_configuracao.key, {
+        expiresIn: 86400,
+    });
+}
 
 rota.post("/registrar", async (req, res) => {
     try {
@@ -18,7 +30,10 @@ rota.post("/registrar", async (req, res) => {
             //APAGA A SENHA ASSIM QUE O USUÁRIO FOR CRIADO
             usuario.senha = undefined;
 
-            return res.send({ usuario });
+            return res.send({
+                usuario,
+                token: gerarToken({ id: usuario.id }),
+            });
         } else {
             return res.status(400).send({ error: "Usuário já cadastrado!" });
         }
@@ -37,19 +52,20 @@ rota.post('/autenticar', async (req, res) => {
     if (usuario) {
         //VERIFICAR SE A SENHA É A MESMA CADASTRADA
         if (await bcryptjs.compare(senha, usuario.senha)) {
-            
+
             //APAGA A SENHA ASSIM QUE O USUÁRIO FOR CRIADO
             usuario.senha = undefined;
 
-            res.send({ usuario });
+            res.send({
+                usuario,
+                token: gerarToken({ id: usuario.id }),
+            });
         } else {
             res.status(400).send({ erro: "Senha inválida." })
         }
     } else {
         res.status(400).send({ erro: "Usuário não encontrado." })
     }
-
-
 });
 
 //PARA RECUPERAR O (app) PASSADO LA NO INDEX
